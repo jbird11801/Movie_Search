a899ae2729a44cb544fe615935ff61f09cca324b
jbird11801@gmail.com <jbird11801@gmail.com>
2023-08-12 11:59:20 -0400

    Made it search on enter

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,6 +4,8 @@ var inputEl = $('#movieInput');
 
 var searchButEl = $('#Search');
 
+var streamDetailsDiv = document.getElementById("stream-details");
+
 var rapidData;
 
 var tmdbData;
@@ -11,20 +13,20 @@ var tmdbData;
 
 //Search btton function
 
-searchButEl.on("click",function(){
+searchButEl.on("click", function () {
 
-var searchInput = inputEl.val();
+    var searchInput = inputEl.val();
 
-findStreamingService( searchInput )
+    findStreamingService(searchInput)
 
-});
+  });
 
-document.addEventListener("keydown", function (event) {
+  document.addEventListener("keydown", function (event) {
 
     var keycode = event.key;
 
     if(keycode === "Enter"){
-        
+
         var searchInput = inputEl.val();
 
         findStreamingService( searchInput )
@@ -32,134 +34,221 @@ document.addEventListener("keydown", function (event) {
 
   });
 
+
 // input the show name and it will out said shows info
 
-function findStreamingService( ShowNameString ) {
+function findStreamingService(ShowNameString) {
 
-    const url = 'https://streaming-availability.p.rapidapi.com/search/title?title='+ ShowNameString +'&country=us&show_type=all&output_language=en';
+    const url = 'https://streaming-availability.p.rapidapi.com/search/title?title=' + ShowNameString + '&country=us&show_type=all&output_language=en';
 
     const options = {
 
-	method: 'GET',
+        method: 'GET',
 
-	headers: {
+        headers: {
 
-		'X-RapidAPI-Key': '89e7e8bb73msha528c083647ebbfp158eedjsne3b14326664d',
+            'X-RapidAPI-Key': '89e7e8bb73msha528c083647ebbfp158eedjsne3b14326664d',
 
-		'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-
-	}
-    
-};
-
-    fetch(url,options)
-
-    .then( function (response){
-
-        return response.json();
-
-    })
-
-    .then(function (data) {
-
-        //the output of the function currently just loging to console
-
-    //    console.log(data);
-
-        rapidData = data.result[0];
-
-        movieDetails(rapidData.imdbId , rapidData.type)
-
-    })
-
-}
-
-function movieDetails(imdb_id , show_type){
-
-const options = {
-
-    method: 'GET',
-
-    headers: {
-
-      accept: 'application/json',
-
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Njk0NjQ2ZTE3ODUyM2E2ODJjY2YxZTA1MmI0YTFkYyIsInN1YiI6IjY0Y2M0NzQwMmYyNjZiMDllZTNiZDM2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._Mfq5m_QU4ApNbHeMUA0d7muVW01_zIoz4gHfUuiTc0'
-    
-    }
-
-  };
-  
-  fetch('https://api.themoviedb.org/3/find/' + imdb_id + '?external_source=imdb_id' , options)
-
-    .then(function(response){
-
-        return response.json()
-
-    })
-
-    .then(function(data){
-
-    //    console.log(data);
-
-        if (show_type === 'movie'){
-
-            tmdbData = data.movie_results[0];
-
-        }else{
-
-            tmdbData = data.tv_results[0];
+            'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
 
         }
 
+    };
 
+    fetch(url, options)
 
-        LogInfo();
+        .then(function (response) {
 
-    })
+            return response.json();
+
+        })
+
+        .then(function (data) {
+
+            //the output of the function currently just loging to console
+
+            //    console.log(data);
+
+            rapidData = data.result[0];
+
+            //console.log(rapidData.streamingInfo);
+
+            var moviesData = []
+
+            //console.log(rapidData);
+
+            for (var i = 0; i < rapidData.streamingInfo.us.length; i++) {
+                var movieInfo = {
+                    website: rapidData.streamingInfo.us[i].service,
+                    type: rapidData.streamingInfo.us[i].streamingType,
+                    quality: rapidData.streamingInfo.us[i].quality,
+                    link: rapidData.streamingInfo.us[i].link
+                };
+                moviesData.push(movieInfo);
+            }
+    
+            for (var i = 0; i < moviesData.length; i++) {
+                var movieElement = createMovieElement(moviesData[i]);
+                streamDetailsDiv.appendChild(movieElement);
+            }
+
+            movieDetails(rapidData.imdbId, rapidData.type)
+
+        })
 
 }
 
-function LogInfo(){
+function movieDetails(imdb_id, show_type) {
 
-   // console.log(rapidData);
+    const options = {
 
-   console.log(tmdbData);
+        method: 'GET',
 
-    console.log('Title')//Title
+        headers: {
 
-    console.log(rapidData.originalTitle);
+            accept: 'application/json',
 
-    console.log('streaming Info')//streaming Info
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Njk0NjQ2ZTE3ODUyM2E2ODJjY2YxZTA1MmI0YTFkYyIsInN1YiI6IjY0Y2M0NzQwMmYyNjZiMDllZTNiZDM2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._Mfq5m_QU4ApNbHeMUA0d7muVW01_zIoz4gHfUuiTc0'
 
-    console.log(rapidData.streamingInfo);
+        }
 
-    console.log('type of content')// type of content 
+    };
 
-    console.log(rapidData.type);
+    fetch('https://api.themoviedb.org/3/find/' + imdb_id + '?external_source=imdb_id', options)
 
-    console.log('release_date')//release date
+        .then(function (response) {
 
-    if (rapidData.type === 'movie'){
+            return response.json()
 
-    console.log(tmdbData.release_date);
+        })
 
-    }
+        .then(function (data) {
 
-    else
+            //    console.log(data);
 
-    {
+            if (show_type === 'movie') {
 
-        console.log(tmdbData.first_air_date);
+                tmdbData = data.movie_results[0];
 
-    }
+            } else {
 
-    console.log('poster')//poster
-        
-    console.log('https://image.tmdb.org/t/p/original/'+ tmdbData.poster_path)
+                tmdbData = data.tv_results[0];
 
-    console.log('desciption')// desciption 
+            }
 
-    console.log(tmdbData.overview);
+            // LogInfo();
+
+            createMovieElement(data);
+
+        })
+
+}
+
+// function LogInfo() {
+
+//     console.log(rapidData);
+
+//     //console.log(tmdbData);
+
+//     console.log('Title')//Title
+
+//     console.log(rapidData.originalTitle);
+
+//     console.log('streaming Info')//streaming Info
+
+//     console.log(rapidData.streamingInfo);
+
+//     console.log(rapidData.streamingInfo.us)
+
+//     console.log('Rent Info')//streaming Info
+
+//     var temp = [];
+
+//     for (var i = 0; i < rapidData.streamingInfo.us.length; i++) {
+//         if (temp.includes(rapidData.streamingInfo.us[i].service)) {
+
+//         }
+
+//         else {
+
+//             temp.push(rapidData.streamingInfo.us[i].service)
+
+//             console.log(rapidData.streamingInfo.us[i].service);
+//             console.log(rapidData.streamingInfo.us[i].streamingType);
+//             console.log(rapidData.streamingInfo.us[i].link);
+
+//         }
+//     };
+
+//     console.log('type of content')// type of content 
+
+//     console.log(rapidData.type);
+
+//     console.log('release_date')//release date
+
+//     if (rapidData.type === 'movie') {
+
+//         console.log(tmdbData.release_date);
+
+//     }
+
+//     else {
+
+//         console.log(tmdbData.first_air_date);
+
+//     }
+
+//     console.log('poster')//poster
+
+//     console.log('https://image.tmdb.org/t/p/original/' + tmdbData.poster_path)
+
+//     console.log('desciption')// desciption 
+
+//     console.log(tmdbData.overview);
+
+// }
+
+
+function createMovieElement(movieData) {
+
+    // if (!(rapidData.streamingInfo.us)){
+
+    //     streamDetailsDiv.textContent = "No streaming services provide this title";
+
+    // }
+    
+    // else 
+    
+    // {
+
+    var site = document.createElement("div");
+
+    site.classList.add("streamcard");
+
+    var service = document.createElement("p");
+
+    var streamingType = document.createElement("p");
+
+    var quality = document.createElement("p");
+
+    var link = document.createElement("link");
+
+    service.textContent = "Service: " + movieData.website;
+
+    streamingType.textContent = "Availability: " + movieData.type;
+
+    quality.textContent = "Quality: " + movieData.quality;
+
+    link.textContent = "Link: " + movieData.link;
+
+    site.appendChild(service);
+
+    site.appendChild(streamingType);
+
+    site.appendChild(quality);
+
+    return site;
+
+   // }
 
 }
